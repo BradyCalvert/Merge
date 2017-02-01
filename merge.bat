0cf36ea9348890b72bcc0adab67e3cb979271d70
svc-student <svc-student@student.directory.jccc>
2017-02-01 13:14:36 -0600

    this is my merge batch file

---
+++ merge.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Unity\Editor\Data\Tools\UnityYAMLMerge.exe" merge -p "Blank.unity" "Red.unity" "Blue.unity" "End.unity"
+"C:\program files\Unity\Editor\Unity.exe" -batchmode -projectPath "C:\Users\svc-student\Desktop\UnityExample"  -buildWindowsPlayer "C:\Users\svc-student\Desktop\UnityExample\End.exe" -quit
